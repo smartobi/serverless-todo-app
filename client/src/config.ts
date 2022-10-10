0c11b53b5118c55dbb44bb9543db9ccec4814ba4
efeokus <efeokujeni@gmail.com>
2022-10-09 18:57:24 -0700

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'zr7hi3rge7';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-8mpjeeif.us.auth0.com',
+  clientId: 'pWoP0KnE9EtGVWD3cn6bOnRmctibVqov',
+  callbackUrl: 'http://localhost:3000/callback'
+};
